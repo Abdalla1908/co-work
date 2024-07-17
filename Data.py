d01dc235f0bf9ded19184a7eadcd2f777c074d70
Abdalla Hassan <bedoseafan@gmail.com>
2024-07-17 23:45:24 +0300

    Saving variables for easy writing

---
+++ Data.py
@@ -0,0 +1,46 @@
+st_opt = """
+1).Register in course
+2).list my courses
+3). view course
+4). Grades Report
+5). Log out
+"""
+sub_st_opt = """
+1. Unregister from course
+2. submit solution
+3. Back
+
+"""
+dr_opt = """ 
+1).List Courses
+2).Create cours
+3). View Cours
+4). Log out
+"""
+sub_dr_opt = """
+1.List Assignments 
+2.Create Assignment 
+3.View Assignment 
+4.Back 
+"""
+
+assignment_opt = """
+1. Show Info
+2. Show Grades Report
+3. List Solutions
+4. View Solution
+5. Back
+"""
+ta_opt = """ 
+1.)Show courses
+2).Invetations
+3).ask for a course
+4).Log out
+"""
+menu_opt = """
+1). Sign in 
+2). Sign up
+3). Shut down
+
+
+"""
